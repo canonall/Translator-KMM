cf3ba48229bbe1506207a82ddcba433611633cd6
onalcan <onal@liftric.com>
2022-12-31 15:19:21 +0100

    remove default greeting/platform code

--- shared/src/commonMain/kotlin/com/canonal/translator/Greeting.kt
+++
@@ -1,9 +0,0 @@
-package com.canonal.translator
-
-class Greeting {
-    private val platform: Platform = getPlatform()
-
-    fun greet(): String {
-        return "Hello, ${platform.name}!"
-    }
-}
--- shared/src/commonMain/kotlin/com/canonal/translator/Platform.kt
+++
@@ -1,7 +0,0 @@
-package com.canonal.translator
-
-interface Platform {
-    val name: String
-}
-
-expect fun getPlatform(): Platform
