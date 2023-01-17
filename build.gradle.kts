9f27cbe222d394905671b6cac6bccd4ee6ca40a0
onalcan <onal@liftric.com>
2023-01-17 20:57:35 +0100

    feature(app): add detekt to the project

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,10 @@
+import io.gitlab.arturbosch.detekt.Detekt
+import io.gitlab.arturbosch.detekt.DetektPlugin
+
+plugins {
+    alias(libs.plugins.detekt)
+}
+
 buildscript {
     repositories {
         gradlePluginPortal()
@@ -9,6 +16,7 @@ buildscript {
         classpath(libs.android.tools.gradle.plugin)
         classpath(libs.sqldelight.gradle.plugin)
         classpath(libs.hilt.gradle.plugin)
+        classpath(libs.detekt.plugin)
     }
 }
 
@@ -16,6 +24,30 @@ allprojects {
     repositories {
         google()
         mavenCentral()
+    }
+}
+
+val detektFormatting = libs.detekt.formatting
+
+subprojects {
+    apply<DetektPlugin>()
+
+    dependencies {
+        detektPlugins(detektFormatting)
+    }
+
+    detekt {
+        parallel = true
+        ignoreFailures = true
+        config = files("$rootDir/config/detekt/detekt.yml")
+    }
+
+    tasks.withType<Detekt>().configureEach {
+        jvmTarget = "1.8"
+        reports {
+            html.required.set(true)
+            xml.required.set(true)
+        }
     }
 }
 
